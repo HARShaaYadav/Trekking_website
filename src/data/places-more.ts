835de26616e924853e7576c04f3082a132104166
HARShaaYadav <160042665+HARShaaYadav@users.noreply.github.com>
2026-05-23 14:30:00 +0530

    info

--- src/data/places-more.ts
+++ src/data/places-more.ts
@@ -83,6 +83,7 @@ export const moreTreks: Trek[] = [
         ],
     },
 ];
+
 export function getMoreTrek(slug: string): typeof moreTreks[0] | undefined {
     return moreTreks.find((trek) => trek.slug === slug);
 }
