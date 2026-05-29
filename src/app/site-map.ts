21518125eb1ae81c7e79a4328e9ca72ef0cc8970
HARShaaYadav <160042665+HARShaaYadav@users.noreply.github.com>
2026-05-29 14:30:00 +0530

    offline map

--- src/app/site-map.ts
+++ src/app/site-map.ts
@@ -63,5 +63,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: "monthly",
             priority: 0.6,
         }));
- return [...staticRoutes, ...trekRoutes, ...blogRoutes];
+ 
+        return [...staticRoutes, ...trekRoutes, ...blogRoutes];
 }
